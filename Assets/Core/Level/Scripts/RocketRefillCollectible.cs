d50e0897f83adc6e81e6d5c0c19db46d347fc6dc
Louis Vogel <louis.vogel@sentry-games.com>
2025-06-14 11:02:10 +0200

    Setup rocket refill

---
+++ Assets/Core/Level/Scripts/RocketRefillCollectible.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class RocketRefillCollectible : MonoBehaviour
+{
+    const string PLAYER_TAG = "Player";
+
+    [SerializeField]
+    float cooldownDuration = 2f;
+
+    [SerializeField]
+    GameObject gfx;
+
+    float cooldownEnd;
+    bool isShown = false;
+
+    public bool CanBePicked => Time.time >= cooldownEnd;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(PLAYER_TAG) && CanBePicked)
+        {
+            other.GetComponent<PlayerRocket>()?.RefillRockets();
+            ToggleCollectibleGFX(false);
+            cooldownEnd = Time.time + cooldownDuration;
+        }
+    }
+
+    private void Update()
+    {
+        if (!isShown && CanBePicked)
+            ToggleCollectibleGFX(true);
+    }
+
+    void ToggleCollectibleGFX(bool toggle)
+    {
+        isShown = toggle;
+
+        gfx.SetActive(toggle);
+    }
+}
--- Assets/Core/Player/Scripts/PlayerRocket.cs
+++ Assets/Core/Player/Scripts/PlayerRocket.cs
@@ -6,17 +6,41 @@ using UnityEngine.InputSystem;
 public class PlayerRocket : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb;
+    [Header("Rocket Refill")]
+    [SerializeField] LayerMask platformLayerMask;
+    [SerializeField] float rocketRefillRaycastDist;
+    [SerializeField] int maxRocketAmount;
+    [SerializeField] float rocketCooldown = .1f;
+    [Header("Rocket Inertia")]
     [SerializeField] float rocketStrength;
     [SerializeField] float baseMomentumMultiplierOnFireIfSameDir = .5f;
     [SerializeField] float momentumMultiplierOnFireSameDirection = .25f;
     [SerializeField] float momentumMultiplierOnFireOppositeDirection = .1f;
+    [Header("Rocket GFX")]
     [SerializeField] Transform rocketLauncher;
 
+    int currentRocketAmount;
+    float nextAllowedRocketShot;
     Vector2 currentRocketLauncherDirection;
+
+    private void Awake()
+    {
+        RefillRockets();
+    }
 
     private void Update()
     {
         rocketLauncher.rotation = Quaternion.Euler(0, 0, Vector2.SignedAngle(Vector2.up, currentRocketLauncherDirection) + 90);
+
+        if (rb.linearVelocityY <= 0 && Physics2D.Raycast(transform.position, Vector2.down, rocketRefillRaycastDist, platformLayerMask))
+        {
+            RefillRockets();
+        }
+    }
+
+    public void RefillRockets()
+    {
+        currentRocketAmount = maxRocketAmount;
     }
 
     public void UpdateRocketLauncherDirection(Vector2 newDirection)
@@ -26,8 +50,7 @@ public class PlayerRocket : MonoBehaviour
 
     public void OnFireClicked()
     {
-        //TODO : check remaining rockets & cooldown
-        if (false)
+        if (currentRocketAmount <= 0 || Time.time < nextAllowedRocketShot)
             return;
 
         FireRocket();
@@ -35,10 +58,8 @@ public class PlayerRocket : MonoBehaviour
 
     private void FireRocket()
     {
-        if (rb != null)
-        {
-
-        }
+        nextAllowedRocketShot = Time.time + rocketCooldown;
+        currentRocketAmount--;
 
         float dotProduct = Vector2.Dot(rb.linearVelocity, -currentRocketLauncherDirection);
 
